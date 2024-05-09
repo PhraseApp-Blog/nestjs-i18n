51ec97d5138413200379207ef8f5557e7ed73b66
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-05-09 10:14:25 +0100

    feat: redirect from / to /info retains headers and query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,32 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   @Get()
-  @Redirect('/info', 301)
-  getRoot() {}
+  getRoot(
+    @Query() query: Record<string, any>,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    // copy original headers
+    Object.entries(req.headers).forEach(
+      ([header, value]) => {
+        res.setHeader(header, value as string);
+      },
+    );
+
+    // copy original query params
+    const queryParams = new URLSearchParams(
+      query,
+    ).toString();
+
+    return res.redirect(302, `/info?${queryParams}`);
+  }
 }
