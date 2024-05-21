b8331e85dd7941658aeaa8591c79f82f73acf38b
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-05-21 14:11:58 +0100

    refactor: removing unncessary header copy when redirecting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,26 +2,17 @@ import {
   Controller,
   Get,
   Query,
-  Req,
   Res,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   @Get()
   getRoot(
     @Query() query: Record<string, any>,
-    @Req() req: Request,
     @Res() res: Response,
   ) {
-    // copy original headers
-    Object.entries(req.headers).forEach(
-      ([header, value]) => {
-        res.setHeader(header, value as string);
-      },
-    );
-
     // copy original query params
     const queryParams = new URLSearchParams(
       query,
